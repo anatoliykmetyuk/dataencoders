03924a564e5a437aa86e397ade64a3c2334547c0
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2016-10-28 20:59:05 +0300

    Download url removed from setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='Data Encoders',
       author='Anatolii Kmetiuk',
       author_email='anatoliykmetyuk@gmail.com',
       url='https://github.com/anatoliykmetyuk/dataencoders',
-      download_url='https://github.com/anatoliykmetyuk/dataencoders/tarball/' + version,
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages())
